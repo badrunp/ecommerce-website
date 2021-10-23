3c568d06d6ffd43f1c7768a44b36e408654ded3c
= <bbadrunn@gmail.com>
2021-10-23 08:35:44 +0700

    create model factory controller migration size

---
+++ database/factories/Backend/SizeFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories\Backend;
+
+use App\Models\Backend\Size;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class SizeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Size::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
