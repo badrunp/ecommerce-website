032b1d3dc673e65b14ac4098cc426d131d747768
= <bbadrunn@gmail.com>
2021-11-19 17:44:47 +0700

    create table product images

---
+++ database/migrations/2021_11_19_104124_create_product_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_images', function (Blueprint $table) {
+            $table->id();
+            $table->text('image');
+            $table->unsignedBigInteger('product_id');
+            $table->timestamps();
+            $table->foreign('product_id')->on('products')->references('id')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_images');
+    }
+}
