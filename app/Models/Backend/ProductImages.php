a86c92205fc0da2425682a7fd8a20bac28556a8c
= <bbadrunn@gmail.com>
2021-11-28 20:59:54 +0700

    create relational product image to product

--- app/Models/Backend/ProductImages.php
+++ app/Models/Backend/ProductImages.php
@@ -8,7 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class ProductImages extends Model
 {
     use HasFactory;
+
     protected $table = "product_images";
+
     protected $fillable = ['image', 'product_id'];
+
+    public function product(){
+
+        return $this->belongsTo(Product::class);
+
+    }
 
 }
